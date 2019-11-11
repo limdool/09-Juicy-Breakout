8dfb0567d67ab88bd370bb53d14a88f02bd260b3
Huiyong Lim <petelim@iu.edu>
2019-11-11 16:51:43 -0500

    Finished Updating

---
+++ Scripts/ball.gd
@@ -0,0 +1,28 @@
+extends RigidBody2D
+
+export var maxspeed = 300
+
+signal lives
+signal score
+var WorldNode 
+
+func _ready():
+ contact_monitor = true
+ set_max_contacts_reported(4)
+ WorldNode = get_node("/root/World")
+ connect("score", WorldNode, "increase_score")
+ connect("lives", WorldNode, "decrease_lives")
+
+func _physics_process(delta):
+ var bodies = get_colliding_bodies()
+ for body in bodies:
+  if body.is_in_group("Tiles"):
+   emit_signal("score",body.score)
+   WorldNode.find_node('Pong').playing = true
+   body.queue_free()
+  if body.get_name() == "Paddle":
+   pass
+  
+ if position.y > get_viewport_rect().end.y:
+  emit_signal("lives")
+  queue_free()
